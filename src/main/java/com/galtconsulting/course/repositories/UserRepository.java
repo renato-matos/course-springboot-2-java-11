7681060e87de3a8065de9ae53630005a89c4f518
Renato Matos <renato.matos@creditas.comb.br>
2020-06-09 14:46:56 -0300

    Service layer, component registration

--- src/main/java/com/galtconsulting/course/repositories/UserRepository.java
+++ src/main/java/com/galtconsulting/course/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.galtconsulting.course.entities.User;
 
+// Não precisa do @Repository porque a interface já extende JpaRepository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
