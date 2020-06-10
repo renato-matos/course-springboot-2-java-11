d63f87e020c4b9301768575ad0247888fd49da37
Renato Matos <renato.matos@creditas.comb.br>
2020-06-10 16:26:04 -0300

    Exception handling - findById

--- src/main/java/com/galtconsulting/course/services/UserService.java
+++ src/main/java/com/galtconsulting/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.galtconsulting.course.entities.User;
 import com.galtconsulting.course.repositories.UserRepository;
+import com.galtconsulting.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
